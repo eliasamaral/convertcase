6e47d2504aba3c2e947110f9af16e4fcf4628abf
eliasamaral <eliasseverianoamaral@gmail.com>
2021-01-07 21:01:30 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+const input = document.getElementById('input')
+const LowCase = document.getElementById('lowcase')
+const UpperCase = document.getElementById('uppercase')
+const iniciaisMaiusculasDOM = document.getElementById('iniciaisMaiusculas')
+const clear = document.getElementById('clear')
+
+
+clear.addEventListener('click', () => {
+    input.value = ''
+})
+
+LowCase.addEventListener('click', () => {
+    tudoMinusculo(input.value)
+})
+
+UpperCase.addEventListener('click', () => {
+    tudoMaiusculo(input.value)
+})
+
+iniciaisMaiusculasDOM.addEventListener('click', () => {
+    iniciaisMaiusculas(input.value)
+})
+
+
+
+const tudoMaiusculo = (e) => {
+    const out = e.toUpperCase()
+    input.value = out
+}
+
+const tudoMinusculo = (e) => {
+    const out = e.toLowerCase()
+    input.value = out
+}
+
+const iniciaisMaiusculas = (e) => {
+    const out = e.toLowerCase().replace(/(?:^|\s)\S/g, function (a) { return a.toUpperCase(); });
+    input.value = out
+}
+
+
+// --------- AJUSTE NA ESTILIZAÇÃO DO CODIGO --------------
+
+
+// var RefDiv = document.getElementById('botoes');
+// console.log(RefDiv);
+// var largura = window.getComputedStyle(RefDiv, null).getPropertyValue("width");
+// input.style.width = largura
+//---------------------------------------------------------
+
+
